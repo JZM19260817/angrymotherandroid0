fb7fec130ee5c51b0ec27eda58e3142852ae79ab
ZHL <314387026@qq.com>
2019-02-15 18:00:44 +0900

    Initial commit

---
+++ app/src/main/java/zhl/shadiaomother/MainActivity.java
@@ -0,0 +1,49 @@
+package zhl.shadiaomother;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public EditText editText1;
+    public EditText editText2;
+    public EditText editText3;
+    public Button button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText1=(EditText)findViewById(R.id.editText1);
+        editText2=(EditText)findViewById(R.id.editText2);
+        editText3=(EditText)findViewById(R.id.editText3);
+        button1=(Button)findViewById(R.id.button1);
+
+
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String txt0="你们这个是什么群啊?你们这是害人不浅啊你们这个群!麻烦你们，真的太过分了!" +
+                        "你们搞这个群干什么?我儿子每一科的成绩都不过那个平均分呐，他现在初二!" +
+                        "你叫我儿子怎么办啊!他还不到高中啊好不好!你们这是什么群啊?你们害死我儿子了!" +
+                        "谁是群主?快点出来你们群主!再不来我去报警了啊!我跟你们说你们这一帮人啊，一天到晚啊搞这些什么"+
+                        editText2.getText().toString()+"啊什么"+editText3.getText().toString()+"啊会害死你们哒！你们没有前途?我跟你说你们这"+
+                        editText1.getText().toString()+"多个人好好学习不好吗，一天到晚在"+editText3.getText().toString()+
+                        "是不是人啊?你们一天到晚"+editText3.getText().toString()+"不好好学习,会没前途的!"+
+                        editText3.getText().toString()+"害死人!";
+                //Toast.makeText(MainActivity.this, txt0, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this,OutActivity.class);
+                //给message起一个名字，并传给另一个activity
+                intent.putExtra("OUTPUT",txt0);
+                //启动意图
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}
---
+++ app/src/main/java/zhl/shadiaomother/OutActivity.java
@@ -0,0 +1,64 @@
+package zhl.shadiaomother;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class OutActivity extends AppCompatActivity {
+    public TextView outText;
+    public Button button2;
+    public Button button3;
+    public Button button4;
+    private ClipboardManager myClipboard;
+    private ClipData myClip;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_out);
+        button2=(Button)findViewById(R.id.button2);
+        button3=(Button)findViewById(R.id.button3);
+        button4=(Button)findViewById(R.id.button4);
+
+        Intent intent=getIntent();
+        String msg=intent.getStringExtra("OUTPUT");
+        outText=(TextView)findViewById(R.id.outText);
+        outText.setText(msg);
+
+        //ClipboardManager cm = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        //Toast.makeText(this, "复制成功，可以发给朋友们了。", Toast.LENGTH_LONG).show();
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String out=outText.getText().toString();
+                myClipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                myClip = ClipData.newPlainText("out",out);
+                myClipboard.setPrimaryClip(myClip);
+                Toast.makeText(OutActivity.this,"复制好了，快去怒斥沙雕群友",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String out="阿姨，您好，本群除了你儿子以外，成员跨越全球，人均985，211毕业，当然还有国外高校的，掌握多种科学技术知识与神学理论，清楚分析全球各国时政新闻，从属行业包括但不限于政治家，数学家，历史学家，计算机行业，航空航天体系，军事指挥体系（绝大部分曾担任国家高级军事指挥官，拥有丰富作战经验）对所有领域都有深入涉及，请问：你儿子算什么东西？";
+                myClipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                myClip = ClipData.newPlainText("out2",out);
+                myClipboard.setPrimaryClip(myClip);
+                Toast.makeText(OutActivity.this,"复制好了，快去怒斥阿姨",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(OutActivity.this,"敬请期待",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
